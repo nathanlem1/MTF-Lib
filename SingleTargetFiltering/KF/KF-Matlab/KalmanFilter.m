96a8d65fe338f8f20ea26cd50cb89a78ebdb3235
Nathanael L. Baisa <nathanaellmss@gmail.com>
2019-01-15 20:56:39 +0000

    KF implementation using Matlab

---
+++ SingleTargetFiltering/KF/KF-Matlab/KalmanFilter.m
@@ -0,0 +1,58 @@
+% This function implements Kalman filter (KF) in a clear and understandable
+% manner. The tutorial for the KF is geiven in the following papers:
+% 1. Greg Welch and Gary Bishop, 'An Introduction to the Kalman Filter'
+% 2. M.S.Arulampalam, S.Maskel, N. Gordon and T Clapp, 'A Tutorial on 
+% Particle Filters for Online Nonlinear/Non-Gaussian Bayesian Tracking'.
+%
+% The state dynamics evolves according to the following equations:
+% x(k) = f(k)(x(k-1), u(k), w(k-1)) = F(k)x(k-1) + G(k)u(k) + w(k-1)
+%                  f(k)(.) is a linear (in KF case) or non-linear) function.
+%                  meaning the state vector x evolves during one time
+%                  step premultiplying by the state transition
+%                  matrix F. There is optionally (if nonzero) an input
+%                  (control) vector u which affects the state linearly, 
+%                  and this linear effect on the state is represented 
+%                  premultiplying by the input matrix G. There is also
+%                  gaussian process noise w. In our case, we set u to 0.
+% where w ~ N(0,Q(k-1)) meaning w(k-1) is gaussian noise with mean zero and
+%                       covariance Q(k-1)
+% The observation model is given by:
+% z(k) = h(k)(x(k),V(k0) = H(k)x(k) + v(k)
+%                  h(k)(.) is a linear (in KF case) or non-linear) function.
+%                  Meaning the observation vector z is a linear function
+%                  of the state vector, and this linear relationship is
+%                  represented by premultiplication of observation
+%                  matrix H. There is also gaussian measurement
+%                  noise v.
+% where v ~ N(0,R(k)) meaning v(k) is gaussian noise with mean zero and
+%                     covariance R(k)
+%
+% Version 1.0, January, 2019
+%
+% This function was written by Nathanael L. Baisa
+% email: nathanaellmss@gmail.com
+%
+
+function [m_update, P_update] = KalmanFilter(z, model, m_update, P_update)
+% Inputs are measurement (z), model (F, Q, H, R), and m_update and P_update 
+% at time k-1.
+% Outputs are m_update and P_update at time k.
+
+% Prediction (time update)
+m_predict = model.F * m_update;
+P_predict = model.Q + model.F*P_update*model.F';
+
+% Compute the covariance (S) of the innovation term z(k)-H(k)*m(k|k-1) and
+% then then Kalman gain (K)
+S = model.H*P_predict*model.H' + model.R;  iS = inv(S);
+K = P_predict*model.H'*iS;
+% S  = model.R+model.H*P_predict*model.H'; Vs= chol(S); det_S= prod(diag(Vs))^2; inv_sqrt_S= inv(Vs);  iS= inv_sqrt_S*inv_sqrt_S';
+% K  = P_predict*model.H'*iS;
+
+% Correction (measurement update)
+m_update = m_predict + K*(z - model.H*m_predict);
+P_update = P_predict - K*model.H*P_predict;
+
+end
+
+
---
+++ SingleTargetFiltering/KF/KF-Matlab/KalmanFilter_demo.m
@@ -0,0 +1,71 @@
+% This script runs KF example
+% State vector is position x,y and velcity vx, vy i.e. [x, vx, y,vy] and 
+% observation vector is position x, y i.e. [x, y]
+
+clc;
+clear;
+close all;
+
+% dynamical model parameters (CV model)
+model.T= 1;                                           %sampling period
+model.A0= [ 1 model.T; 0 1 ];                                            
+model.F= [ model.A0 zeros(2,2); zeros(2,2) model.A0 ]; %transition matrix  
+model.B0= [ (model.T^2)/2; model.T ];
+model.B= [ model.B0 zeros(2,1); zeros(2,1) model.B0 ]; %process noise standard deviation
+model.sigma_v = 1.5;
+model.Q= (model.sigma_v)^2* model.B*model.B';   %process noise covariance
+
+% observation model parameters (noisy x/y only)
+model.H= [ 1 0 0 0 ; 0 0 1 0 ];    %observation matrix
+model.D= diag([ 10; 10 ]);         %observation noise standard deviation
+model.R= model.D*model.D';         %observation noise covariance
+
+% Birth parameters 
+m_birth = [ 10; 0; -10; 0 ];   %mean of Gaussian birth term 
+B_birth = diag([ 10; 10; 10; 10 ]); %std of Gaussian birth term
+P_birth = B_birth*B_birth';  %cov of Gaussian birth term
+
+xstart  = m_birth; %[ 0; 0; 0; -10 ]; 
+targetstate = xstart;
+
+N_duration = 100; %length of data/number of scans
+truth_X= cell(N_duration,1);  %ground truth for state of target
+meas_Z= cell(N_duration,1);   %generated measurement of a target
+
+% Initialization
+m_update = m_birth;
+P_update = P_birth;
+
+% Storing estimated states
+estimated_X = cell(N_duration,1);
+
+figure(1), hold on;
+
+for k = 1:N_duration
+    %targetstate = model.F * targetstate + sqrt(model.Q)*randn(4,1); % generate ground truth for states of a target
+    W = model.sigma_v*model.B*randn(size(model.B,2),size(targetstate,2)); % equivalent to 'sqrt(model.Q)*randn(4,1)'
+    targetstate = model.F * targetstate + W;  % generate ground truth for states of a target
+    truth_X{k}= targetstate;
+    
+    %meas_Z{k}= model.H*targetstate + sqrt(model.R)*randn(size(model.R,2),1); % generate measurement of a target
+    V = model.D*randn(size(model.D,2),size(targetstate,2)); % equivalent to 'sqrt(model.R)*randn(size(model.R,2),1)'
+    meas_Z{k}= model.H*targetstate + V; % generate measurement of a target
+    
+    [m_update, P_update] = KalmanFilter(meas_Z{k}, model, m_update, P_update);
+    
+    estimated_X{k} = m_update;
+    
+    %Plot the ground truth
+    plot(truth_X{k}(1), truth_X{k}(3), '.-r', 'LineWidth', 2);
+    %Plot the measurement
+    plot(meas_Z{k}(1), meas_Z{k}(2), '.-b', 'LineWidth', 2);
+    %Plot the estimated state
+    plot(estimated_X{k}(1), estimated_X{k}(3), '.-g', 'LineWidth', 2);
+    
+end
+
+
+
+
+
+
