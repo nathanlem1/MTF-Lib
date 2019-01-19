87f7b8d67c74824e790de59bbc82147f779860ec
Nathanael L. Baisa <nathanaellmss@gmail.com>
2019-01-19 21:06:02 +0000

    Update unscentedTransform.m

--- SingleTargetFiltering/UKF/UKF-Matlab/unscentedTransform.m
+++ SingleTargetFiltering/UKF/UKF-Matlab/unscentedTransform.m
@@ -1,9 +1,10 @@
-function [X,w]= unscentedTransform(m,P,alpha,kappa)
+function [X,W]= unscentedTransform(m,P,alpha,kappa)
 
 % standard unscented transformation - note [choose alpha=1 => =lambda=kappa; giving offset of beta for first cov weight]
 
-n_x= length(m);
-lambda= alpha^2*(n_x+kappa) - n_x;
-Psqrtm= chol((n_x+lambda)*P)';
-X= repmat(m,[1 2*n_x+1])+ [ zeros(n_x,1) -Psqrtm Psqrtm ];
-w= [ lambda 0.5*ones(1,2*n_x) ]/(n_x+lambda);
+n_x = length(m); % Dimension of the new state vector which is the concatenation of the state and observation vectors. 
+lambda = alpha^2*(n_x + kappa) - n_x;
+Psqrtm = chol((n_x + lambda)*P)';% Calculate matrix square root of weighted covariance matrix. This is better than Psqrtm = real(sqrt((n_x + lambda)*P))  
+X = repmat(m,[1 2*n_x+1])+ [ zeros(n_x,1) -Psqrtm Psqrtm ];    % Array of the sigma points
+W = [ lambda 0.5*ones(1,2*n_x) ]/(n_x+lambda);    % Array of the weights for each sigma point
+
