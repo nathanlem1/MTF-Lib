8049f196d78f522648616c3f38b7484227f71327
Nathanael L. Baisa <nathanaellmss@gmail.com>
2019-08-26 22:12:12 +0100

    Delete gaus_prune_1.m

--- RFS_tracking_algorithms/_common/gaus_prune_1.m
+++
@@ -1,8 +0,0 @@
-function [w_new,x_new,P_new,u_new,v_new]= gaus_prune_1(w,x,P,u,v,elim_threshold)
-
-idx= find( w > elim_threshold );
-w_new= w(idx);
-x_new= x(:,idx);
-P_new= P(:,:,idx);
-u_new= u(idx);
-v_new= v(idx);
